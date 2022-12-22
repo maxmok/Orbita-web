38045c8b0d6f6b9ddb91123915f78e089017eaa2
admin <admin@TECLAST>
2022-12-22 09:38:55 +0400

    [add] add full

--- models/Person.php
+++ models/Person.php
@@ -63,4 +63,67 @@ class Person extends \yii\db\ActiveRecord
     {
         return new PersonQuery(get_called_class());
     }
+    
+    public static function getDays():array {        
+        $list = [];
+        $list[0] = '';
+        for ($i=1; $i<=31; $i++){
+            $list[$i] = $i;        
+        }                    
+        return $list;
+    }
+    
+    public static function getMonths():array {
+        $months = self::find()
+                ->select(["to_char(birth_date, 'TMMonth') as name", "EXTRACT(month FROM birth_date) as index"])
+                ->orderBy('index')
+                ->distinct()
+                ->column();
+        $list = [];
+        $list[0] = '';
+        foreach($months as $index => $month) {
+            $list[$index+1] = $month;
+        }
+        return $list;
+        
+//        $list[1] = 'Январь';
+//        $list[2] = 'Февраль';
+//        $list[3] = 'Март';
+//        $list[4] = 'Апрель';
+//        $list[5] = 'Май';
+//        $list[6] = 'Июнь';
+//        $list[7] = 'Июль';
+//        $list[8] = 'Август';
+//        $list[9] = 'Сентябрь';
+//        $list[10] = 'Октябрь';
+//        $list[11] = 'Ноябрь';
+//        $list[12] = 'Декабрь';        
+    }
+    
+    public static function getYears():array {
+        $years = self::find()
+                ->select("EXTRACT(Year FROM birth_date) as index")
+                ->orderBy('index')
+                ->distinct()
+                ->column();
+        $list = [];
+        $list[0] = '';
+        foreach($years as $year) {
+            $list[$year] = $year;
+        }
+        return $list;
+        
+//        $list[1] = 'Январь';
+//        $list[2] = 'Февраль';
+//        $list[3] = 'Март';
+//        $list[4] = 'Апрель';
+//        $list[5] = 'Май';
+//        $list[6] = 'Июнь';
+//        $list[7] = 'Июль';
+//        $list[8] = 'Август';
+//        $list[9] = 'Сентябрь';
+//        $list[10] = 'Октябрь';
+//        $list[11] = 'Ноябрь';
+//        $list[12] = 'Декабрь';        
+    }
 }
